4893ccdc0a14e1e6cf7625d72f131113dfc0478e
Kofi Owusu <k.o360@live.com>
2023-09-19 16:55:01 +0000

    first commit

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+/* Function prototypes for helper functions */
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+/**
+* _printf - Produces output according to a format.
+* @format: The format string.
+*
+* Return: The number of characters printed (excluding the null byte).
+*/
+int _printf(const char *format, ...)
+{
+va_list args;
+int count = 0;
+int i = 0;
+va_start(args, format);
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+if (format[i] == '\0')
+return (-1); /* Incomplete format specifier */
+if (format[i] == 'c')
+count += print_char(args);
+else if (format[i] == 's')
+count += print_string(args);
+else if (format[i] == '%')
+count += print_percent(args);
+else if (format[i] == 'd' || format[i] == 'i')
+count += print_int(args);
+else
+count += _putchar('%') + _putchar(format[i]);
+}
+else
+{
+count += _putchar(format[i]);
+}
+i++;
+}
+va_end(args);
+return (count);
+}
---
+++ helper_functions_2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/* Function prototypes for helper functions */
+int print_percent(va_list args);
+int print_int(va_list args);
+/**
+* print_percent - Print a percent character.
+* @args: The argument list.
+*
+* Return: Number of characters printed.
+*/
+int print_percent(va_list args)
+{
+(void)args; /* Unused parameter */
+return (_putchar('%'));
+}
+/**
+* print_int - Print an integer.
+* @args: The argument list.
+*
+* Return: Number of characters printed.
+*/
+int print_int(va_list args)
+{
+int num = va_arg(args, int);
+char buffer[12]; /* Assumes a 32-bit integer. */
+int len = _itoa(num, buffer);
+int count = 0;
+while (buffer[count])
+{
+_putchar(buffer[count]);
+count++;
+}
+return (len);
+}
