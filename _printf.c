4723860f5ed3e28761d60351cad0d5bccbe65c8e
Kofi Owusu <k.o360@live.com>
2023-09-22 17:16:03 +0000

    first commit

--- _printf.c
+++ _printf.c
@@ -1,24 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
-* _printf - Produces output according to a format...
-* @format: Is a character string. The format string...
-* is composed of zero or more directives
+* _printf - function my printf
+* @format: string whit format to print
 *
-* Return: The number of characters printed (excluding
-* the null byte used to end output to strings)
-**/
+* Return: number of chars that print
+*/
 int _printf(const char *format, ...)
 {
-int size;
 va_list args;
+int length = 0;
 if (format == NULL)
 return (-1);
-size = _strlen(format);
-if (size <= 0)
-return (0);
 va_start(args, format);
-size = handler(format, args);
-_putchar(-1);
+length = _print_format(format, args);
 va_end(args);
-return (size);
+return (length);
 }
--- main.h
+++ main.h
@@ -1,50 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
-
 /**
- * struct _format - Typedef struct...
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
+  * struct specifiers - Struct specifiers
+  * @specifier: The conversion specifier
+  * @f: The function pointer
+  */
+typedef struct specifiers
 {
-	char type;
-	int (*f)(va_list);
-} format;
+	char *specifier;
+	int (*f)(va_list args);
+} spc_dt;
 
+int _write(char c);
+int _printf(const char *format, ...);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+int _print_spec(char format, va_list args);
+int _print_invalid_spec(char prev_format, char format, int count);
+int _print_a_integer(va_list args);
+void _recursion_integer(int a);
+int _print_int_binary(va_list args);
+void _recursion_int_binary(int a);
+int _validate_char(char _type);
 
-#endif /* MAIN_H */
+#endif 
