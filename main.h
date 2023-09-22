6473d67e5667353f69433526a5c4d034b1b8942c
Kofi Owusu <k.o360@live.com>
2023-09-22 16:53:36 +0000

    first commit

--- main.h
+++ main.h
@@ -1,44 +1,50 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
-/**
- * struct Pf_func - format
- * _printf- print function
- * @op: pointer
- * @f: function pointer
-*/
-typedef struct Pf_func
-{
-	char *op;
-	int (*f)(va_list);
-} print_fun;
-/**
- * _printf - Entry function
- * @format: operator
- * Return: function
- */
-int _printf(const char *format, ...); /*prototype printf*/
-/**
- *_putchar-printing character
- * @c:character to be printed
- * Return: 0
- */
-int _putchar(char c); /*prototype putchar*/
-int _strcmp(const char *s1, char *s2);
-int (*get_op_func(const char *s, int pos))(va_list);
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
 int print_binary(va_list);
-int _pow_recursion(int x, int y);
-int print_single_char(va_list c);
-int print_string_char(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_decimal(va_list d);
-int the_range(unsigned int n);
-/*prototype function that compares two strings.*/
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
+
+/**
+ * struct _format - Typedef struct...
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
+
 #endif /* MAIN_H */
